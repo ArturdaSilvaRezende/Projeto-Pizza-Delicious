e479def3815b32c0d1507bc243e1dbba0dcab2d7
Artur da Silva Rezende <artursrezende@gmail.com>
2020-06-03 10:15:41 -0300

    Javascript map implementado e responsivo, área de contato finalizada

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@
 'use strict';
 
 //O bloco de código abaixo, faz o controle do show e hide do about-index
-$(document).ready(()=> {
+$(document).ready(() => {
 
     const about_pizza_delicious = window.matchMedia("(max-width: 1024px)");
     const aboutHall = $("#about-index");//Esta variável pega todo o container
     const aboutContainer = $("#about-index-content");//Esta variável o conteúdo da box
-    
+
     abount_control_show();
-   
+
 
     function abount_control_show() {
 
@@ -27,7 +27,6 @@ $(document).ready(()=> {
 
     }
 
-    
-})
 
+})
 
---
+++ js/map.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//O bloco de código abaixo, faz o controle do javascript api
+let platform = new H.service.Platform({
+    'apikey': 'l1BwF1EMH485FGaU91NVsaY1CWw7VcGSeC2LTlDWltY'
+});
+
+// Retrieve the target element for the map:
+let targetElement = document.getElementById('mapContainer');
+
+// Obtain the default map types from the platform object:
+let defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+let map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+        zoom: 10,
+        center: { lat: 52.5, lng: 13.4 }
+    });
+
+// Este bloco de código controla o dimensionamento do mapa
+var ui = H.ui.UI.createDefault(map, defaultLayers, 'de-DE');
+var mapSettings = ui.getControl('mapsettings');
+var zoom = ui.getControl('zoom');
+var scalebar = ui.getControl('scalebar');   
+mapSettings.setAlignment('top-left');
+zoom.setAlignment('top-left');
+scalebar.setAlignment('top-left');
+
+
+// Get an instance of the geocoding service:
+var service = platform.getSearchService();
+
+// Call the geocode method with the geocoding parameters,
+// the callback and an error callback function (called if a
+// communication error occurs):
+service.geocode({
+    q: 'Rua x-9, Quadra 16, Lote 15, American Park, Aparecida de Goiânia, Goiás, Brazil' //Seu endereço 
+}, (result) => {
+    // Add a marker for each location found
+    result.items.forEach((item) => {
+        map.addObject(new H.map.Marker(item.position));
+        map.setCenter(item.position);
+        map.setZoom(16);
+    });
+}, alert);
+
